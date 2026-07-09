05657b1cda49667410d514a8e814403431216a60
maxxfuu <maxfu826@gmail.com>
2026-07-09 11:33:10 -0700

    more blind practice to get the intuition of writing correct CUDA

--- src/vectoradd.cu
+++ src/vectoradd.cu
@@ -1,5 +1,5 @@
-#include <cstdio>
-#include <cuda_runtime.h>
+//include <cstdio>
+//include <cuda_runtime.h>
 
 __global__ void vectorAdd(float *a, float *b, float *c) {
     int i = threadIdx.x;
@@ -7,36 +7,36 @@ __global__ void vectorAdd(float *a, float *b, float *c) {
 }
 
 int main() {
-    # n is the number of elements in the array
+    // n is the number of elements in the array
     int n = 8;
-    # bytes is the size of the array in bytes
+    // bytes is the size of the array in bytes
     size_t bytes = n * sizeof(float);
     
-    # allocate memory on the host for the arrays
+    // allocate memory on the host for the arrays
     float *h_a = (float*)malloc(bytes);
     float *h_b = (float*)malloc(bytes);
     float *h_c = (float*)malloc(bytes);
 
-    # initialize the arrays with values 
+    // initialize the arrays with values 
     for (int i = 0; i < n; ++i) {
         h_a[i] = (float)i;
         h_b[i] = (float)(i * 2);
     }
 
-    # allocate memory on the device for the arrays
+    // allocate memory on the device for the arrays
     float *d_a, *d_b, *d_c;
     cudaMalloc((void**)&d_a, bytes);
     cudaMalloc((void**)&d_b, bytes);
     cudaMalloc((void**)&d_c, bytes);
 
-    # copy the arrays from the host to the device
+    // copy the arrays from the host to the device
     cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
 
-    # launch the kernel -> kernel code is defined under the global execution specifer 
+    // launch the kernel -> kernel code is defined under the global execution specifer 
     vectorAdd<<<1, 8>>>(d_a, d_b, d_c);
 
-    # copy the results back into the device
+    // copy the results back into the device
     cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
 
     int success = 1;
@@ -53,7 +53,7 @@ int main() {
         printf("All elements are correct.\n");
     }
 
-    # free the memory that was allocated on the host and device
+    // free the memory that was allocated on the host and device
     free(h_a);
     free(h_b);
     free(h_c);
