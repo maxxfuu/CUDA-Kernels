cd85efc95d578e7c80e1186a93025899488247de
maxxfuu <maxfu826@gmail.com>
2026-07-21 20:09:14 -0700

    CUDA and CUBLAS macros for error handling

--- mnist-kernel/mnist.cu
+++ mnist-kernel/mnist.cu
@@ -4,7 +4,26 @@
 #include <time.h>
 #include <cuda_runtime.h>
 #include <cublas_v2.h>
-#define CUDA_CHECK(call) do { cudaError_t error = call; if (error != cudaSuccess) { fprintf(stderr, "CUDA error at %s:%d: %s\n", __FILE__, __LINE__, cudaGetErrorString(error)); cudaDeviceReset(); exit(EXIT_FAILURE); } } while(0)
+
+#define CUDA_CHECK(call) \
+    do { \
+        cudaError_t error = call; \
+        if (error != cudaSuccess) { \
+            fprintf(stderr, "CUDA error at %s:%d: %s (%d)\n", __FILE__, __LINE__, \
+                    cudaGetErrorString(error), error); \
+            cudaDeviceReset(); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
+
+#define CUBLAS_CHECK(call) \
+    do { \
+        cublasStatus_t status = call; \
+        if (status != CUBLAS_STATUS_SUCCESS) { \
+            fprintf(stderr, "cuBLAS error at %s:%d: %d\n", __FILE__, __LINE__, status); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while(0)
 
 #define INPUT_SIZE 784
 #define HIDDEN_SIZE 256
